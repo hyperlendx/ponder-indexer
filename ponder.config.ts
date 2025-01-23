e7c9daebe86f7942d277ba159ca3289744838fc2
fbslo <admin@fbslo.net>
2025-01-23 16:31:19 +0000

    update start block

--- ponder.config.ts
+++ ponder.config.ts
@@ -18,13 +18,13 @@ export default createConfig({
             address: [
                 "0x1e85CCDf0D098a9f55b82F3E35013Eda235C8BD8", //main pool
             ],
-            startBlock: 9610948,
+            startBlock: 16650240,
         },
         Oracle: {
             network: "hyperEvmTestnet",
             abi: OracleAbi,
             address: "0xecbD8482C698B7b2706807A32d7FDf4E9a55C6A1", //main pool oracle
-            startBlock: 9610948,
+            startBlock: 16650240,
         }
     },
 });
