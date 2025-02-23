b342e224ab9a59974c004b4c4a2c5a8f8342e550
fbslo <admin@fbslo.net>
2025-02-23 18:31:56 +0100

    feat: add mainnet config

--- ponder.config.ts
+++ ponder.config.ts
@@ -7,38 +7,38 @@ import { IsolatedAbi } from "./abis/IsolatedAbi";
 
 export default createConfig({
     networks: {
-        hyperEvmTestnet: {
-            chainId: 998,
-            transport: http(process.env.PONDER_RPC_URL_998),
+        hyperEvm: {
+            chainId: 999,
+            transport: http(process.env.PONDER_RPC_URL_999),
         },
     },
     contracts: {
         CorePool: {
-            network: "hyperEvmTestnet",
+            network: "hyperEvm",
             abi: CorePoolAbi,
             address: [
-                "0x1e85CCDf0D098a9f55b82F3E35013Eda235C8BD8", //main pool
+                "0x036Ad31A37b747e39322878eD851711507f13b1b", //main pool
             ],
-            startBlock: 16956624,
+            startBlock: 249000,
         },
         Oracle: {
-            network: "hyperEvmTestnet",
+            network: "hyperEvm",
             abi: OracleAbi,
-            address: "0xecbD8482C698B7b2706807A32d7FDf4E9a55C6A1", //main pool oracle
-            startBlock: 16956624,
+            address: "0x6C6188e608809E328274f1B57C0112A41e83Cd55", //main pool oracle
+            startBlock: 249000,
         },
         IsolatedPair: {
             abi: IsolatedAbi,
-            network: "hyperEvmTestnet",
+            network: "hyperEvm",
             address: factory({
               // The address of the factory contract that creates instances of this child contract.
-              address: "0x274396Ec36D17dAbC018d9437D5a4C0D0fD503D0",
+              address: "0x9A32C32D7A0e13892Cd68E143AC890F6308304F5",
               // The event emitted by the factory that announces a new instance of this child contract.
               event: parseAbiItem("event AddPair(address pairAddress)"),
               // The name of the parameter that contains the address of the new child contract.
               parameter: "pairAddress",
             }),
-            startBlock: 16956624,
+            startBlock: 249000,
         }
     },
 });
